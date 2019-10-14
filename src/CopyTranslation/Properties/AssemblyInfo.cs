ff59e080715665f1b040bc73a9745a49186663b4
Magentaize <peoleser@gmail.com>
2019-10-14 21:34:53 +0800

    add copyright

--- src/CopyTranslation/Properties/AssemblyInfo.cs
+++ src/CopyTranslation/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CopyTranslation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CopyTranslation")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyCopyright("Copyright © Magentaize. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
